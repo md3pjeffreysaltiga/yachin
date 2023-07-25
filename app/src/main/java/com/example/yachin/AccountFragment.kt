454f3f75d28560646e5d2054c98ee82217ca3ade
rizz24 <rizu2402@gmail.com>
2023-07-25 17:34:44 +0800

    register

--- app/src/main/java/com/example/yachin/AccountFragment.kt
+++ app/src/main/java/com/example/yachin/AccountFragment.kt
@@ -1,16 +1,18 @@
 package com.example.yachin
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.yachin.databinding.FragmentAccountBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class AccountFragment() : Fragment()  {
     public var HomeFragment: AccountFragment = this
 
-
+    private var firebaseAuth = FirebaseAuth.getInstance()
     private var _binding: FragmentAccountBinding? = null
     private val binding get() = _binding!!
 
@@ -26,7 +28,21 @@ class AccountFragment() : Fragment()  {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.idincluderegister.registerButton.setOnClickListener {
+ //           firebaseAuth.signInWithEmailAndPassword( binding.idincluderegister.etEmail.text.toString(),
+            firebaseAuth.createUserWithEmailAndPassword( binding.idincluderegister.etEmail.text.toString(),
+                binding.idincluderegister.etPassword.text.toString()  ).addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    binding.layoutAccount.visibility=View.VISIBLE
+                    binding.layoutRegister.visibility=View.GONE
+                    Log.e("Riz","Login")
+                } else {
+                    binding.layoutAccount.visibility=View.GONE
+                    binding.layoutRegister.visibility=View.VISIBLE
 
+                }
+            }
+        }
 
     }
 
